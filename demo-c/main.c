5731bf931e5cb1f015445a759bb7b3dd003c5552
Addison <63932310+addiment@users.noreply.github.com>
2024-11-11 20:08:53 -0500

    minor repo tweaks

--- demo-c/main.c
+++ demo-c/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include "../avk.h"
 
-AVK_EXPORT void avk_main() {
+AvkRaw *init_rom() {
     Image images[MAX_IMAGES];
     Palette palettes[MAX_PALETTES];
 
     AvkRaw *avk = avk_init(images, palettes);
+    return avk;
+}
+
+AVK_EXPORT void avk_main() {
+    AvkRaw *avk = init_rom();
 
     while (avk_update(avk)) {
         puts("hi!");
